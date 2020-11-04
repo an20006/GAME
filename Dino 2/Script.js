0cfae2adedff7371880f8470daad447b6248b2c9
Taha Khan <tk2004@hw.ac.uk>
2020-11-04 17:16:11 +0300

    Dino 2 update

--- Dino 2/Script.js
+++ Dino 2/Script.js
@@ -6,3 +6,17 @@ audiogo = new Audio('gameover.mp3');
 setTimeout(() => {
     audio.play()
 }, 1000);
+document.onkeydown = function (e) {
+    console.log("Key code is: ", e.keyCode)
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
