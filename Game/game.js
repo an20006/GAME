331c90a76e0e81b2e3663e924a249828a16b3195
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-13 13:13:50 +0400

    Added score system

--- Game/game.js
+++ Game/game.js
@@ -1,30 +1,30 @@
 var dino = document.getElementById("dino");
 var dragon = document.getElementById("dragon");
 function dinoJump() {
-    if (dino.classList != "animateDino"){
-        dino.classList.add("animateDino");
+    if (dino.classList == "animateDino"){
+        return
     }
+    dino.classList.add("animateDino");
     setTimeout(function(){
         dino.classList.remove("animateDino");
     }, 500);
 }
 
 var scoreIncrease = 0;
-var score = document.getElementById("score");
 var ifHit = setInterval(function(){
     var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     var dragonLeft = parseInt(window.getComputedStyle(dragon).getPropertyValue("left"));
     if (dragonLeft<20 && dragonLeft>0 && dinoTop>= 130){
         dragon.style.animation = "none";
         dragon.style.display = "none";
-        alert("You Lose!")
+        alert("You Lose! Your Score is " + parseInt(scoreIncrease))
     }
-    if (dragonLeft == -40){
-        scoreIncrease += 1;
+    else {
+        scoreIncrease += 1/100;
         increaseScore(scoreIncrease)
     }
 }, 10)
 
 function increaseScore(scoreIncrease){
-    document.getElementById("score") = "Score : " + scoreIncrease;
+    document.getElementById("score").innerHTML = "Score : " + parseInt(scoreIncrease);
 }
