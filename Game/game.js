ad2b0cb8cf355b2d1cbe5a30296e94fff2da2824
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-13 04:00:49 +0400

    Completed basic game

---
+++ Game/game.js
@@ -0,0 +1,20 @@
+var dino = document.getElementById("dino");
+var dragon = document.getElementById("dragon");
+function dinoJump() {
+    if (dino.classList != "animateDino"){
+        dino.classList.add("animateDino");
+    }
+    setTimeout(function(){
+        dino.classList.remove("animateDino");
+    }, 500);
+}
+
+var ifHit = setInterval(function(){
+    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    var dragonLeft = parseInt(window.getComputedStyle(dragon).getPropertyValue("left"));
+    if (dragonLeft<20 && dragonLeft>0 && dinoTop>= 130){
+        dragon.style.animation = "none";
+        dragon.style.display = "none";
+        alert("You Lose!")
+    }
+}, 10)
