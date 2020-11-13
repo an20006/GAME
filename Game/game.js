2f84af2714e711762243262236de5215582119ae
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-13 05:22:03 +0400

    Added some code to implement a score

--- Game/game.js
+++ Game/game.js
@@ -9,6 +9,8 @@ function dinoJump() {
     }, 500);
 }
 
+var scoreIncrease = 0;
+var score = document.getElementById("score");
 var ifHit = setInterval(function(){
     var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     var dragonLeft = parseInt(window.getComputedStyle(dragon).getPropertyValue("left"));
@@ -17,4 +19,12 @@ var ifHit = setInterval(function(){
         dragon.style.display = "none";
         alert("You Lose!")
     }
+    if (dragonLeft == -40){
+        scoreIncrease += 1;
+        increaseScore(scoreIncrease)
+    }
 }, 10)
+
+function increaseScore(scoreIncrease){
+    document.getElementById("score") = "Score : " + scoreIncrease;
+}
