f097d2eeedb250d071f8af7fd2a9c0c952fd9e3d
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-25 16:09:08 +0400

    Leaderboard score syntax changed

--- Game/game.js
+++ Game/game.js
@@ -102,7 +102,7 @@ setInterval(function(){ // Ends game and also increases score
         dead = true;
         gameOverMusic.play();
         gameOver.innerHTML = "Game Over! Press Restart"
-        update_leaderboard(  currentplayer , scoreIncrease );
+        update_leaderboard(  currentplayer , parseInt(scoreIncrease));
         delete scoreIncrease; moveObjects = undefined;
         return;
     }
@@ -122,12 +122,12 @@ function get_player() {
     currentplayer = prompt( "Please enter your name", "Harry Potter" );
 }
 
-function update_leaderboard( player,scoreIncrease ) { 
+function update_leaderboard( player,score ) { 
     jQuery.ajax({
         url:"functions.php",    //the page containing php script
         type: "post",    //request type,
         dataType: 'json',
-        data: { player: player, score: scoreIncrease, action:'add_score' },
+        data: { player: player, score: score, action:'add_score' },
         success:function(result){
             console.log(result.result);
         }
