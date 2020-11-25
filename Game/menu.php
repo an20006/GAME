f8eda01c27ad7a48f8b160728756f445ee3bc02c
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-25 19:08:13 +0400

    Added main menu css

--- Game/menu.php
+++ Game/menu.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Menu</title>
+    <link rel="stylesheet" href="game.css">
 </head>
 <body>
 <div id="gameContainer">
